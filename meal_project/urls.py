662171e2f67a5defa6cdd18d0560b81db7840da6
yayuyokano <reckonyuyo@gmail.com>
2025-11-07 03:03:49 +0900

    Clean up duplicate files

--- meal_project/urls.py
+++
@@ -1,8 +0,0 @@
-"""プロジェクト全体のURLルーティング。"""
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("calculator.urls")),
-]
